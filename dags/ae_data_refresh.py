75afe77bab17146adb0302167341ec09a79d3b2b
Vagos Pertsinis <evangelos.pertsinis@altusgroup.com>
2020-11-12 19:23:23 +0200

    Change time delay for DAG retry to 1min

--- dags/ae_data_refresh.py
+++ dags/ae_data_refresh.py
@@ -33,7 +33,7 @@ args = {
     "email_on_failure": False,
     "email_on_retry": False,
     "retries": 1,
-    "retry_delay": timedelta(minutes=5)
+    "retry_delay": timedelta(minutes=1)
 }
 
 snowflake_connection = {
